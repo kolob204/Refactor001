446a9b57c3f9a896a849a6d661d82fbba18e61f2
Viktor <elastic@yandex.ru>
2019-11-09 16:17:00 +0400

    refactor step4: create metod gettotalamount()

--- src/com/movie/Customer.java
+++ src/com/movie/Customer.java
@@ -13,7 +13,8 @@ public class Customer {
         this._name = name;
     }
 
-    //Объект - Арендатор, будет список аренды
+    // Опишем событие - "Арендатор взял фильм в прокат"
+    // Объект - Арендатор, будет список аренды
     public void addRental(Rental arg) {
         //исправлена опечатка кода в книге
         _rentals.addElement(arg);
@@ -39,7 +40,7 @@ public class Customer {
     //структурирован удобным для добавления этой функциональности образом, сначала произведите рефакторинг
     //программы, чтобы упростить внесение необходимых изменений, а только потом добавьте функцию.
     public String statement() {
-        int totalAmount = 0;
+
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result ="Учет аренды для : " + get_name() + "\n";
@@ -51,6 +52,8 @@ public class Customer {
 
             // убрали временную переменную thisAmount,  и присваиваем выход each.getCharge(); там где это нужно - напрямую
             //старайтесь избаляться от временных переменных
+            //однако теперь each.getCharge();  считается дважды.
+            //следующим шагом, мы вынесем этот подсчёт в отдельный метод
 
 
             //выделим так же начисление бонусов в отдельный метод. и поместим его в класс Rental, потому что
@@ -60,17 +63,25 @@ public class Customer {
 //показать результаты для этой аренды
             result += "\t" + each.get_movie().get_title()+ "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
+
         } // WHILE
 
 //добавить нижний колонтитул
         result += "Сумма задолженности составляет: " +
-                String.valueOf(totalAmount) + "\n";
+                String.valueOf(gettotalamount()) + "\n";
         result += "Вы заработали " + String.valueOf(frequentRenterPoints) +
 " очка(ов) за активность";
         return result;
 
     }  // statement()
 
-
+   int gettotalamount() {
+        int result=0;
+       Enumeration rentals = _rentals.elements();
+       while (rentals.hasMoreElements()) {
+           Rental each = (Rental) rentals.nextElement();
+           result+=each.getCharge();
+       }
+        return result;
+   }
 }
